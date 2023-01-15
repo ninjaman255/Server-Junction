777b481d5a12e9f9706c96ce44a95efed6e54e96
ninjaman255 <aaronclevette@gmail.com>
2023-01-15 11:59:34 -0600

    Added Super Sekret Club

--- Server_6_25_2022_Server_Junction/assets/tiles/gate.tsx
+++ Server_6_25_2022_Server_Junction/assets/tiles/gate.tsx
@@ -1,5 +1,6 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.4" tiledversion="1.4.1" name="gate" tilewidth="34" tileheight="52" tilecount="5" columns="5" objectalignment="bottom">
+<tileset version="1.8" tiledversion="1.8.6" name="gate" tilewidth="34" tileheight="52" tilecount="5" columns="5" objectalignment="bottom">
+ <tileoffset x="1" y="8"/>
  <grid orientation="isometric" width="64" height="32"/>
  <properties>
   <property name="Solid" type="bool" value="true"/>
@@ -7,7 +8,7 @@
  <image source="gate.png" width="170" height="52"/>
  <tile id="0">
   <objectgroup draworder="index" id="3">
-   <object id="3" x="7" y="22.5" width="33" height="32"/>
+   <object id="3" x="0" y="32" width="33" height="32"/>
   </objectgroup>
   <animation>
    <frame tileid="0" duration="50"/>
